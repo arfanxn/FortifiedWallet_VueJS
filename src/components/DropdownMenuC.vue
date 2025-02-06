75269f50b807f0e03bd76409c22ca6ed0a8b4156
arfanxn <arfan2173@gmail.com>
2025-02-06 20:06:08 +0700

    created dropdown menu

--- src/components/DropdownMenuC.vue
+++ src/components/DropdownMenuC.vue
@@ -2,7 +2,7 @@
   <button class="relative" @click="isDropdownOpened = !isDropdownOpened">
     <FontAwesomeIcon icon="user" class="text-xl" />
     <ul
-      class="absolute right-0 min-w-44 origin-top-right flex-col items-start overflow-auto rounded-lg border-2 border-slate-700 bg-white transition duration-300 ease-in-out md:min-w-64"
+      class="absolute right-0 min-w-44 origin-top-right flex-col items-start overflow-auto rounded-lg border-2 border-slate-200 bg-slate-700 transition duration-300 ease-in-out md:min-w-64"
       :class="{
         'flex opacity-100': isDropdownOpened,
         'hidden opacity-0': !isDropdownOpened,
@@ -11,7 +11,7 @@
       <li v-for="(menu, index) in props.menus" :key="index" class="w-full">
         <button
           @click="menuOnClick(menu)"
-          class="inline-flex w-full items-center justify-between gap-x-2 px-2 py-1 text-slate-700 hover:bg-slate-200 hover:text-slate-800"
+          class="inline-flex w-full items-center justify-between gap-x-2 px-2 py-1 text-slate-300 hover:bg-slate-600 hover:text-slate-200"
         >
           <FontAwesomeIcon
             v-if="menu.icon"
