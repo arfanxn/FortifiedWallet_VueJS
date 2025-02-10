559667f8a4d30efdd6277772ff8d63980f14f59f
arfanxn <arfan2173@gmail.com>
2025-02-10 13:57:18 +0700

    add two methods on string utils

--- src/utils/string.utils.js
+++ src/utils/string.utils.js
@@ -11,3 +11,27 @@ export function contains(string, substring) {
   const regex = new RegExp(`.*${substring}.*`)
   return regex.test(string)
 }
+
+/**
+ * Checks if a given string is not empty and not null.
+ *
+ * @param {string} string - The string to check.
+ * @returns {boolean} Whether the string is not empty and not null.
+ */
+export function notEmpty(string) {
+  return string != '' && string != null
+}
+
+
+/**
+ * Formats an Ethereum address to a shortened version for display purposes.
+ *
+ * This function takes an Ethereum address and returns a string with the first
+ * 6 characters, followed by an ellipsis, and the last 4 characters of the address.
+ *
+ * @param {string} address - The Ethereum address to format.
+ * @returns {string} The formatted Ethereum address.
+ */
+export function formatEthereumAddress(address) {
+  return `${address.slice(0, 6)}...${address.slice(-4)}`;
+}
