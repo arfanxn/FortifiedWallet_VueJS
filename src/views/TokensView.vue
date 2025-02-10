a7feed5f2b8ed596f1f872617a62ee34bd654931
arfanxn <arfan2173@gmail.com>
2025-02-11 05:48:09 +0700

    created tokens view

---
+++ src/views/TokensView.vue
@@ -0,0 +1,10 @@
+<template>
+  <AuthLayout> </AuthLayout>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import AuthLayout from '@/layouts/AuthLayout.vue'
+
+onMounted(() => {})
+</script>
