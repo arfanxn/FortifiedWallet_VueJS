00e5fbb59dd6c53ca61d7f61284f7a69ebc064fa
arfanxn <arfan2173@gmail.com>
2025-02-11 08:49:15 +0700

    convert store into compositon api

--- src/stores/toast.store.js
+++ src/stores/toast.store.js
@@ -1,7 +1,11 @@
 // stores/ethers.ts
+import { ref } from 'vue'
 import { defineStore } from 'pinia';
 
-export const useToastStore = defineStore('toast', {
+export const useToastStore = defineStore('toast', () => {
+  // ==========================================================================
+  //                                State
+  // ==========================================================================
   /**
    * The state of the toast store.
    *
@@ -11,54 +15,60 @@ export const useToastStore = defineStore('toast', {
    * @property {string} message - The message to be displayed in the toast.
    * @property {number} disappearIn - The time in milliseconds before the toast disappears.
    */
-  state: () => ({
-    toasts: [
-    ],
-  }),
-  getters: {
-    //
-  },
-  actions: {
+  const toasts = ref([])
 
-    /**
-     * Adds a new toast notification to the store with a specified type, message, and duration.
-     *
-     * @param {string} type - The type of the toast, which can be 'info', 'success', 'warning', or 'error'.
-     * @param {string} message - The message to display within the toast notification.
-     * @param {number} [disappearIn=3000] - The duration in milliseconds before the toast automatically disappears.
-     *                                      This value is adjusted based on the duration of the previous toast if one exists.
-     */
 
-    addToast(type, message, disappearIn = 3000) {
-      const id = crypto.randomUUID()
+  // ==========================================================================
+  //                                Actions
+  // ==========================================================================
+  /**
+   * Adds a new toast notification to the store with a specified type, message, and duration.
+   *
+   * @param {string} type - The type of the toast, which can be 'info', 'success', 'warning', or 'error'.
+   * @param {string} message - The message to display within the toast notification.
+   * @param {number} [disappearIn=3000] - The duration in milliseconds before the toast automatically disappears.
+   *                                      This value is adjusted based on the duration of the previous toast if one exists.
+   */
+  const addToast = (type, message, disappearIn = 3000) => {
+    const id = crypto.randomUUID()
 
-      const toastsLength = this.toasts.length;
-      if (toastsLength > 0)
-        disappearIn += this.toasts[toastsLength - 1].disappearIn
+    const toastsLength = toasts.value.length;
+    if (toastsLength > 0)
+      disappearIn += toasts.value[toastsLength - 1].disappearIn
 
-      this.toasts.push({
-        id,
-        type,
-        message,
-        disappearIn
-      })
+    toasts.value.push({
+      id,
+      type,
+      message,
+      disappearIn
+    })
 
-      setTimeout(() => {
-        this.removeToast(id)
-      }, disappearIn)
-    },
+    setTimeout(() => {
+      removeToast(id)
+    }, disappearIn)
+  }
+  /**
+   * Removes a toast notification with the specified id from the store.
+   *
+   * @param {string} id - The id of the toast to remove.
+   */
+  const removeToast = (id) => {
+    const index = toasts.value.findIndex(toast => toast.id === id);
+    if (index !== -1) {
+      toasts.value.splice(index, 1);
+    }
+  }
 
-    /**
-     * Removes a toast notification with the specified id from the store.
-     *
-     * @param {string} id - The id of the toast to remove.
-     */
-    removeToast(id) {
-      const index = this.toasts.findIndex(toast => toast.id === id);
-      if (index !== -1) {
-        this.toasts.splice(index, 1);
-      }
-    },
-  },
+  // ==========================================================================
+  //                                  Initializations
+  // ==========================================================================
+
+  // ==========================================================================
+  //                                Returns the store
+  // ==========================================================================
+  return {
+    toasts,
+    addToast,
+    removeToast
+  }
 })
-
