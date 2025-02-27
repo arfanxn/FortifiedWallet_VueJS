f31b8bdbda5926e8df99c077c0bda54ce250400e
arfanxn <arfan2173@gmail.com>
2025-02-27 07:47:37 +0700

    options api to composition api

--- src/stores/navigation.store.js
+++ src/stores/navigation.store.js
@@ -1,37 +1,44 @@
 // stores/ethers.ts
 import { defineStore } from 'pinia';
+import { ref } from 'vue';
 
-export const useNavigationStore = defineStore('navigation', {
-  state: () => ({
-    isSidebarOpened: false,
-    menus: [
-      {
-        path: '/',
-        name: '0xDashboard',
-        symbol: '0xH',
-        icon: 'house',
-      },
-      {
-        path: '/tokens',
-        name: '0xTokens',
-        symbol: '0xT',
-        icon: 'coins',
-      },
-      {
-        path: '/transactions',
-        name: '0xTransactions',
-        symbol: '0xT',
-        icon: 'right-left',
-      },
-    ],
-  }),
-  getters: {
-    //
-  },
-  actions: {
-    toggleSidebar() {
-      this.isSidebarOpened = !this.isSidebarOpened
+export const useNavigationStore = defineStore('navigation', () => {
+  // ==========================================================================
+  //                                State
+  // ==========================================================================
+  const isSidebarOpened = ref(false)
+  const menus = ref([
+    {
+      path: '/',
+      name: '0xDashboard',
+      symbol: '0xH',
+      icon: 'house',
     },
-  },
-})
+    {
+      path: '/tokens',
+      name: '0xTokens',
+      symbol: '0xT',
+      icon: 'coins',
+    },
+    {
+      path: '/transactions',
+      name: '0xTransactions',
+      symbol: '0xT',
+      icon: 'right-left',
+    },
+  ])
 
+  // ==========================================================================
+  //                                Getters
+  // ==========================================================================
+  //
+
+  // ==========================================================================
+  //                                Actions
+  // ==========================================================================
+  const toggleSidebar = () => {
+    isSidebarOpened.value = !isSidebarOpened.value
+  }
+
+  return { isSidebarOpened, menus, toggleSidebar }
+})
