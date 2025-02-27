d4c20ada8fdcab9697e14bdf8ca8a1d589c621c3
arfanxn <arfan2173@gmail.com>
2025-02-27 18:49:05 +0700

    added tryRethrow

--- src/utils/error.utils.js
+++ src/utils/error.utils.js
@@ -14,3 +14,19 @@ export function tryCatchFinally(tryClosure, catchClosure, finallyClosure = null)
     if (finallyClosure instanceof Function) finallyClosure()
   }
 }
+
+/**
+ * Execute a closure in a try block and rethrow any thrown errors.
+ *
+ * @param {Function} tryClosure - The closure to execute in the try block.
+ * @returns {any} The return value of the try closure.
+ * @throws {Error} The thrown error from the try closure.
+ */
+export function tryRethrow(tryClosure) {
+  // eslint-disable-next-line no-useless-catch
+  try {
+    return tryClosure()
+  } catch (error) {
+    throw error
+  }
+}
