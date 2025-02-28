a559bebb38753082f589529a8f08cb035b97994b
arfanxn <arfan2173@gmail.com>
2025-02-28 22:07:23 +0700

    created route helpers

---
+++ src/helpers/route.helpers.js
@@ -0,0 +1,10 @@
+import { useRoute } from "vue-router"
+
+export function getParentRoute() {
+  const route = useRoute()
+  return route.matched[route.matched.length - 2]
+}
+
+export function getParentRouteMeta() {
+  return getParentRoute().meta
+}
