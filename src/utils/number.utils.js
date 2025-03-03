948f08c435bad138c20932f66bfe542ba3f29ee8
arfanxn <arfan2173@gmail.com>
2025-03-04 04:51:56 +0700

    added number util functions

--- src/utils/number.utils.js
+++ src/utils/number.utils.js
@@ -11,3 +11,14 @@ export function extractNumber(string) {
   const number = String(string).replace(/\D/g, ''); // "a1b2c3!@#" -> 123
   return number
 }
+
+export function toNumber(string, radix = 10, fallback) {
+  const number = parseInt(string, radix);
+  if (Number.isInteger(number)) return number
+  else if (fallback != undefined) return fallback
+  else return number
+}
+
+export function toBase10Number(string, fallback) {
+  return toNumber(string, 10, fallback)
+}
