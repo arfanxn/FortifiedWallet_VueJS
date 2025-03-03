c48fc73b59d5306a05d36bfdf4b98774a7496afa
arfanxn <arfan2173@gmail.com>
2025-03-03 16:29:54 +0700

    created reusable components

---
+++ src/components/PaginationButtonC.vue
@@ -0,0 +1,45 @@
+<template>
+  <button
+    :class="{
+      'cursor-not-allowed opacity-75': props.disabled,
+      'cursor-pointer': !props.disabled,
+    }"
+    :disabled="props.disabled"
+    @click="onClick"
+  >
+    <FontAwesomeIcon
+      :icon="isLeftDirection ? faSquareCaretLeft : faSquareCaretRight"
+      class="text-xl"
+    />
+  </button>
+</template>
+
+<script setup>
+import { defineProps, defineEmits, computed } from 'vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faSquareCaretLeft, faSquareCaretRight } from '@fortawesome/free-solid-svg-icons'
+
+const emit = defineEmits(['onClick'])
+
+function onClick() {
+  emit('onClick')
+}
+
+const props = defineProps({
+  direction: { type: String, required: true }, // direction can be 'prev' or 'next'
+  disabled: { type: Boolean, required: true },
+})
+
+const isLeftDirection = computed(() => {
+  switch (props.direction) {
+    case 'prev':
+    case 'previous':
+    case 'left':
+      return true
+    case 'next':
+    case 'right':
+    default:
+      return false
+  }
+})
+</script>
---
+++ src/components/wallets/WalletListItemC.vue
@@ -0,0 +1,45 @@
+<template>
+  <li
+    class="inline-flex cursor-pointer items-center justify-start gap-x-4 px-4 py-2 hover:bg-slate-400 hover:text-slate-800"
+    :class="{
+      'bg-slate-700 text-slate-300 hover:bg-slate-700! hover:text-slate-300!':
+        route.params.walletAddr === props.wallet.address,
+    }"
+    @click="() => onItemClick(props.wallet)"
+  >
+    <FontAwesomeIcon :icon="faUser" class="text-xl" />
+    <div class="flex flex-col items-start overflow-hidden text-ellipsis">
+      <span class="font-mono">{{ formatEthAddr(props.wallet.address) }}</span>
+      <span class="inline-flex items-center">
+        <FontAwesomeIcon :icon="faDollarSign" class="text-sm" />
+        <span>{{ formatUsd(props.wallet.totalBalanceInUsd) }}</span>
+      </span>
+    </div>
+  </li>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+import { useRoute } from 'vue-router'
+import { formatEthAddr, formatUsd } from '@/helpers/string.helpers'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faDollarSign, faUser } from '@fortawesome/free-solid-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+library.add(faDollarSign, faUser)
+
+const route = useRoute()
+
+const emit = defineEmits(['onItemClick'])
+
+const props = defineProps({
+  wallet: {
+    type: Object,
+    required: true,
+  },
+})
+
+function onItemClick(wallet) {
+  emit('onItemClick', wallet)
+}
+</script>
