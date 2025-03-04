78c8c51ede3f69e446980b4c4c2efa627671cbc7
arfanxn <arfan2173@gmail.com>
2025-03-05 06:43:43 +0700

    fixed select field bug

--- src/components/SelectFieldC.vue
+++ src/components/SelectFieldC.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="flex flex-col gap-y-1 text-slate-700">
-    <label class="font-semibold" :for="inputId" v-if="notEmpty(props.label)">{{
-      props.label
-    }}</label>
+    <label class="font-semibold" :for="inputId" v-if="props.label">{{ props.label }}</label>
     <select
       class="w-full rounded-lg border border-slate-600 px-4 py-2 transition-all focus:border-transparent focus:ring-2 focus:ring-slate-600 focus:outline-none"
       :name="props.name"
@@ -13,8 +11,12 @@
       @blur="emit('onBlur', $event)"
       @focus="emit('onFocus', $event)"
     >
-      <option v-for="option in props.options" :key="option.key" :value="option.value">
-        {{ option.key }}
+      <option
+        v-for="option in props.options"
+        :key="option.key ?? option.value"
+        :value="option.value"
+      >
+        {{ option.text }}
       </option>
     </select>
   </div>
@@ -24,7 +26,7 @@
 import { notEmpty } from '@/utils/string.utils'
 import { defineComponent, defineModel, defineEmits, computed, onMounted, Ref, ref } from 'vue'
 import type { ObjectKeyValue } from '@/interfaces/interfaces'
-import { SelectFieldProps } from '@/interfaces/componentProps.interfaces'
+import { SelectFieldComponent } from '@/interfaces/component.interfaces'
 
 defineComponent({
   name: 'SelectFieldC',
@@ -38,33 +40,8 @@ onMounted(() => {
 const inputId: Ref<string> = ref('')
 const model = defineModel()
 
-// const props = defineProps({
-//   name: {
-//     type: String,
-//     required: true,
-//   },
-//   options: {
-//     type: ObjectKeyValue[],
-//     required: true,
-//   },
-//   label: {
-//     type: String,
-//     default: null,
-//   },
-//   disabled: {
-//     type: Boolean,
-//     default: false,
-//   },
-//   value: {
-//     type: String,
-//     default: null,
-//   },
-// })
-
-const props = withDefaults(defineProps<SelectFieldProps<number>>(), {
-  label: null,
+const props = withDefaults(defineProps<SelectFieldComponent.Props<any>>(), {
   disabled: false,
-  value: null,
   options: () => [],
 })
 </script>
