53abbf8f0005495504018e9d70a617315f9608fc
arfanxn <arfan2173@gmail.com>
2025-03-05 06:43:17 +0700

    renamed and more interfaces

---
+++ src/interfaces/component.interfaces.ts
@@ -0,0 +1,19 @@
+import type { ObjectKeyValue, StringOrNull } from '@/interfaces/interfaces'
+
+export namespace SelectFieldComponent {
+  export interface Props<T> {
+    name: string
+    label?: string
+    disabled?: boolean
+    value?: T
+    options: SelectFieldComponent.Props.Option<T>[]
+  }
+
+  export namespace Props {
+    export interface Option<T> {
+      text: string
+      key?: string
+      value: T
+    }
+  }
+}
--- src/interfaces/componentProps.interfaces.ts
+++
@@ -1,9 +0,0 @@
-import type { ObjectKeyValue, StringOrNull } from '@/interfaces/interfaces'
-
-export interface SelectFieldProps<T> {
-  name: string
-  label: StringOrNull
-  disabled: boolean
-  value: StringOrNull
-  options: ObjectKeyValue<T>[]
-}
