837cf1fbe19f6fe8bc2d614db3ee336fe0a2cb43
arfanxn <arfan2173@gmail.com>
2025-03-09 15:49:28 +0700

    minor update

--- src/services/ethereum.service.ts
+++ src/services/ethereum.service.ts
@@ -3,12 +3,13 @@ import { MetamaskNotInstalledError } from '../errors/ethereum.errors'
 import { ethers } from 'ethers'
 
 /**
- * Connects to the Ethereum network using MetaMask as a provider.
+ * Connects to the Ethereum provider by first checking if MetaMask is installed
+ * and then creates an ethers.js provider object.
  *
- * @returns {Promise<ethers.BrowserProvider>} A promise that resolves to an ethers.js provider object.
- * @throws {Error} If MetaMask is not installed or available in the browser.
+ * @returns {Promise<ethers.BrowserProvider>} A promise that resolves to the provider
+ * object.
+ * @throws {MetamaskNotInstalledError} If MetaMask is not installed.
  */
-
 export const connect = async (): Promise<ethers.BrowserProvider> => {
   if (!window.ethereum || !window.ethereum.isMetaMask) throw new MetamaskNotInstalledError()
   const provider = new ethers.BrowserProvider(window.ethereum)
@@ -16,13 +17,12 @@ export const connect = async (): Promise<ethers.BrowserProvider> => {
 }
 
 /**
- * Requests the connected Ethereum accounts from the provider.
+ * Retrieves the list of Ethereum accounts available through the provider.
  *
- * @param {Promise<ethers.BrowserProvider>} provider An ethers.js provider object.
- * @returns {Promise<string[]>} A promise that resolves to an array of connected
- * Ethereum account addresses.
+ * @param {ethers.BrowserProvider} provider - An ethers.js provider object.
+ * @returns {Promise<string[]>} A promise that resolves to an array of account addresses.
  */
-export const getAccounts = async (provider: ethers.BrowserProvider) => {
+export const getAccounts = async (provider: ethers.BrowserProvider): Promise<string[]> => {
   const accounts = await provider.send('eth_requestAccounts', [])
   return accounts
 }
