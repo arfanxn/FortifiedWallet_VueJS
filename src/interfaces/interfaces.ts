ee815c42c6e02f9b7f7d35574809bbe139a65a98
arfanxn <arfan2173@gmail.com>
2025-03-10 22:06:30 +0700

    added vue types

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -1,3 +1,5 @@
+import type { Ref, ComputedRef } from 'vue'
+
 export type StringOrNull = string | null
 export type StringOrUndefined = string | undefined
 export type StringOrNullOrUndefined = string | null | undefined
@@ -8,6 +10,9 @@ export type BooleanOrNullOrUndefined = boolean | null | undefined
 
 export type NumberOrNull = number | null
 export type NumberOrNullOrUndefined = number | null | undefined
+
+export type ComputedRefOrRef<T> = ComputedRef<T> | Ref<T>
+export type RawOrComputedRefOrRef<T> = T | ComputedRefOrRef<T>
 
 export interface ObjectKeyValue<T> {
   key: string
