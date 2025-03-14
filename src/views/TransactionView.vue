98215be98b3f63e5bd2a482c6ff3c83e4924316f
arfanxn <arfan2173@gmail.com>
2025-03-15 00:44:03 +0700

    simplify and refactored

--- src/views/TransactionView.vue
+++ src/views/TransactionView.vue
@@ -1,34 +1,52 @@
 <template>
+  <!-- Main authenticated layout wrapping wallet and transaction views -->
   <AuthLayout>
-    <main class="flex items-start">
-      <WalletIndex @walletOnClick="(wallet) => filterTransactionByWallet(wallet)" />
-      <div class="flex w-full flex-col">
-        <TransactionIndex />
+    <!-- Responsive container for wallet list and transaction content -->
+    <div class="flex flex-col items-start md:flex-row">
+      <!-- Wallet list sidebar with search and pagination controls -->
+      <WalletIndex
+        class="w-full md:basis-1/4"
+        v-model:keyword="walletStore.keyword"
+        v-model:currentPage="walletStore.currentPage"
+        v-model:selectedWallet="walletStore.selectedWallet"
+        :wallets="walletStore.wallets"
+        @onSelect="navigateToTransactionIndex"
+        @onDeselect="navigateToTransaction"
+        @onPaginate="navigateToTransaction"
+        @onFind="navigateToTransactionIndex"
+      />
+
+      <!-- Main content area for transaction views -->
+      <div class="flex w-full flex-col md:basis-3/4">
+        <!-- Dynamic view container for transaction details/index -->
+        <router-view />
       </div>
-    </main>
+    </div>
   </AuthLayout>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { onMounted } from 'vue'
-import AuthLayout from '@/layouts/AuthLayout.vue'
-import { useRoute, useRouter } from 'vue-router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faLink } from '@fortawesome/free-solid-svg-icons'
-import { useEthereumStore } from '@/stores/ethereum.store'
+import AuthLayout from '@/layouts/AuthLayout.vue'
 import WalletIndex from '@/components/wallets/WalletIndex.vue'
-import TransactionIndex from '@/components/transactions/TransactionIndex.vue'
+import { useWalletStore } from '@/stores/wallet.store'
+import { useWalletInteraction } from '@/composables/wallets/walletInteraction.composable'
+import { useTransactionNavigator } from '@/composables/transactions/useTransactionNavigator'
+import { useAppUI } from '@/composables/appUI.composable'
 
 library.add(faLink)
 
-const route = useRoute()
-const router = useRouter()
+const walletStore = useWalletStore()
+const { withLoading } = useAppUI()
+const { navigateToTransaction, navigateToTransactionIndex } = useTransactionNavigator()
+const { syncWalletStoreWithRoute, syncTransactionStoreWithRoute } = useWalletInteraction()
 
-let ethereumStore = useEthereumStore()
-
-function filterTransactionByWallet(address) {
-  router.push({ path: '/transactions', query: { wallet: address } })
-}
-
-onMounted(() => {})
+onMounted(() => {
+  withLoading(async () => {
+    await syncWalletStoreWithRoute()
+    await syncTransactionStoreWithRoute()
+  })
+})
 </script>
