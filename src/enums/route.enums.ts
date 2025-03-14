3b4a5d1c03301712fc14ca5fdf2773e9ab6004df
arfanxn <arfan2173@gmail.com>
2025-03-14 18:49:08 +0700

    added more route names

--- src/enums/route.enums.ts
+++ src/enums/route.enums.ts
@@ -17,7 +17,9 @@ export enum RouteName {
   TokenIndex = 'token.index',
 
   // Transaction relateds
+  Transaction = 'transaction',
   TransactionIndex = 'transaction.index',
+  TransactionShow = 'transaction.show',
 
   // User relateds
   NotificationIndex = 'notification.index',
