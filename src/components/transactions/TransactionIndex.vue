37b0f93fa33b6b484eb314b6d5871be93b1a6d82
arfanxn <arfan2173@gmail.com>
2025-03-15 12:53:36 +0700

    updated TransactionIndex.vue

--- src/components/transactions/TransactionIndex.vue
+++ src/components/transactions/TransactionIndex.vue
@@ -1,115 +1,93 @@
 <template>
-  <section class="flex flex-col bg-slate-300 text-slate-700 outline-1">
-    <header class="inline-flex items-center justify-between px-4 py-4 outline-1 md:px-4">
+  <section class="flex flex-col gap-y-4 bg-slate-300 text-slate-700">
+    <header class="inline-flex items-center justify-between px-4 pt-4 md:px-4">
       <h2 class="text-lg font-bold">Transactions</h2>
     </header>
 
-    <div class="inline-flex px-4 py-4 md:gap-x-4">
-      <TextFieldC class="grow" name="transaction_hash" placeholder="Transaction hash (0x1234...)" />
-      <ButtonC :icon="faMagnifyingGlass" text="Search" class="rounded-lg" />
+    <form class="inline-flex px-4 md:gap-x-2" @submit.prevent>
+      <TextFieldC
+        @onKeyupEnter="() => handleFindSubmission()"
+        v-model="transactionStore.keyword"
+        class="grow"
+        name="transaction_hash"
+        placeholder="Transaction hash (0x1234...)"
+      />
+      <ButtonC
+        @onClick="() => handleFindSubmission()"
+        type="button"
+        text="Find"
+        class="rounded-lg outline-0!"
+      />
+    </form>
+
+    <!-- Message displayed when no wallets are found -->
+    <div
+      v-if="transactionStore.transactions.length === 0 && !isLoading && ethereumStore.isConnected"
+      class="flex items-center justify-center"
+    >
+      <span class="text-lg font-semibold">No Transaction(s) Found.</span>
     </div>
 
-    <ul class="flex flex-col items-start gap-y-4 font-medium">
-      <li
-        class="grid w-full cursor-pointer gap-x-4 px-4 py-2 transition-all duration-200 hover:bg-slate-400 hover:text-slate-800 md:grid-cols-4"
-        v-for="(transaction, index) in Array(8).fill('TX-xxxx')"
+    <ul
+      v-if="walletStore.selectedWallet && transactionStore.transactions.length > 0 && !isLoading"
+      class="flex flex-col items-start gap-y-4 font-medium"
+    >
+      <TransactionListItem
+        v-for="(transaction, index) in transactionStore.transactions"
         :key="index"
+        :wallet="walletStore.selectedWallet!"
+        :transaction="transaction"
+        :isHovered="hoveredTransactionIndex === index"
         @mouseenter="hoveredTransactionIndex = index"
         @mouseleave="hoveredTransactionIndex = null"
-      >
-        <div class="flex flex-col gap-y-1 text-sm md:col-span-3">
-          <div class="inline-flex items-center gap-x-2 font-semibold">
-            <FontAwesomeIcon :icon="faHashtag" class="w-4 text-sm" />
-            <span>{{ `0x5b9c2f8e1d4a6c3b0a7e5f9d2c8a1b6e4f3a0d9c7b2e5f8a3d1c6b9e4f7a2d0` }}</span>
-          </div>
-
-          <div class="inline-flex items-center gap-x-2">
-            <FontAwesomeIcon :icon="faArrowRightArrowLeft" class="w-4 text-sm" />
-            <span class="font-semibold">{{
-              formatEthAddr(`0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266`)
-            }}</span>
-            <FontAwesomeIcon :icon="faArrowRightLong" class="w-4 text-sm" />
-            <span class="font-semibold">{{
-              formatEthAddr(`0xf39Fd6e51aad88F6F4ce6aB8827279cffFb944e4`)
-            }}</span>
-          </div>
-
-          <div class="inline-flex items-center gap-x-2 font-semibold">
-            <FontAwesomeIcon :icon="faCoins" class="w-4 text-sm" />
-            <span>{{ `0xf39Fd6e51aad88F6F4ce6aB8827279cffFb94567` }}</span>
-          </div>
-
-          <div class="flex items-center gap-x-2 font-semibold">
-            <FontAwesomeIcon :icon="faHourglassHalf" class="w-4 text-sm" />
-            <span>{{ `Pending` }}</span>
-          </div>
-        </div>
-
-        <div class="flex basis-1/4 flex-col justify-between gap-y-1 text-sm md:col-span-1">
-          <div class="ml-auto inline-flex items-center gap-x-2 font-semibold text-red-700">
-            <FontAwesomeIcon :icon="faMinus" class="w-4 text-sm" />
-            <span class="inline-flex items-center"
-              ><FontAwesomeIcon :icon="faDollarSign" class="w-4 text-sm" />{{ `0.789` }}</span
-            >
-          </div>
-        </div>
-
-        <div class="flex items-center justify-between text-sm font-semibold md:col-span-4">
-          <div class="flex items-center gap-x-2">
-            <FontAwesomeIcon :icon="faCalendar" class="w-4 text-sm" />
-            <span>{{ `12-12-2020` }}</span>
-          </div>
-
-          <div
-            class="flex items-center justify-end gap-x-2"
-            v-if="hoveredTransactionIndex === index"
-          >
-            <ButtonC
-              @onClick="() => {}"
-              type="button"
-              :text="'Cancel'"
-              class="bg-red-700! text-slate-200! hover:bg-red-600! hover:text-slate-100!"
-            />
-            <!-- <ButtonC
-            @onClick="() => {}"
-            type="button"
-            :text="'Revoke'"
-            class="bg-amber-700! text-slate-200! hover:bg-amber-600! hover:text-slate-100!"
-          /> -->
-            <ButtonC
-              @onClick="() => {}"
-              type="button"
-              :text="'Approve'"
-              class="bg-green-700! text-slate-200! hover:bg-green-600! hover:text-slate-100!"
-            />
-            <ButtonC @onClick="() => {}" type="button" :text="'Execute'" />
-          </div>
-        </div>
-      </li>
+      />
     </ul>
+
+    <!-- Pagination controls -->
+    <div
+      v-if="!transactionStore.selectedTransaction"
+      class="flex items-center justify-end gap-x-2 px-4"
+    >
+      <span class="mr-4 text-sm font-semibold">Page: {{ transactionStore.currentPage }}</span>
+      <PaginationButtonC
+        direction="prev"
+        :disabled="transactionStore.currentPage === 1"
+        @onClick="() => handlePaginate(transactionStore.currentPage - 1)"
+      />
+      <PaginationButtonC
+        direction="next"
+        :disabled="transactionStore.transactions.length === 0"
+        @onClick="() => handlePaginate(transactionStore.currentPage + 1)"
+      />
+    </div>
   </section>
 </template>
 
 <script setup lang="ts">
-import { defineComponent, ref } from 'vue'
-import { formatEthAddr } from '@/helpers/string.helpers'
-import TextFieldC from '@/components/TextFieldC.vue'
-import ButtonC from '@/components/ButtonC.vue'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import {
-  faArrowRightLong,
-  faCoins,
-  faHashtag,
-  faHourglassHalf,
-  faMinus,
-  faDollarSign,
-  faArrowRightArrowLeft,
-  faMagnifyingGlass,
-  faCalendar,
-} from '@fortawesome/free-solid-svg-icons'
+import { defineComponent, onMounted, onUnmounted, ref } from 'vue'
+import { string } from 'yup'
+import { isEthHash } from '@/utils/boolean.utils'
 import { library } from '@fortawesome/fontawesome-svg-core'
-
-const hoveredTransactionIndex = ref<Number | null>(null)
+import { useAppUI } from '@/composables/appUI.composable'
+import { useTransactionStore } from '@/stores/transaction.store'
+import { useWalletStore } from '@/stores/wallet.store'
+import ButtonC from '@/components/ButtonC.vue'
+import TextFieldC from '@/components/TextFieldC.vue'
+import TransactionListItem from '@/components/transactions/TransactionListItem.vue'
+import PaginationButtonC from '@/components/PaginationButtonC.vue'
+import { showToast } from '@/helpers/toast.helpers'
+import { ToastType } from '@/enums/toast.enums'
+import {
+  faArrowRightArrowLeft,
+  faArrowRightLong,
+  faCalendar,
+  faCoins,
+  faDollarSign,
+  faHashtag,
+  faMinus,
+} from '@fortawesome/free-solid-svg-icons'
+import { useEthereumStore } from '@/stores/ethereum.store'
+import { useTransactionNavigator } from '@/composables/transactions/useTransactionNavigator'
 
 library.add(
   faArrowRightLong,
@@ -121,9 +99,55 @@ library.add(
   faCalendar,
 )
 
-// let ethereumStore = useEthereumStore()
-
 defineComponent({
   name: 'WalletShow',
 })
+
+const ethereumStore = useEthereumStore()
+const walletStore = useWalletStore()
+const transactionStore = useTransactionStore()
+const { isLoading, startLoading, stopLoading } = useAppUI()
+const { navigateToTransactionIndex } = useTransactionNavigator()
+const hoveredTransactionIndex = ref<Number | null>(null)
+
+const keywordSchema = string()
+  .optional()
+  .test(
+    'length',
+    'Transaction hash must be 66 characters.',
+    (value) => !value || value.length === 66,
+  )
+  .test('ethereum-hash', 'Transaction hash is invalid.', (value) => !value || isEthHash(value))
+
+onMounted(() => {
+  //
+})
+
+onUnmounted(() => {
+  transactionStore.reset()
+})
+
+async function handlePaginate(page: number) {
+  transactionStore.currentPage = page
+  navigateToTransactionIndex()
+}
+
+async function handleFindSubmission() {
+  try {
+    startLoading()
+
+    keywordSchema.validateSync(transactionStore.keyword)
+
+    if (transactionStore.keyword) {
+      navigateToTransactionIndex({ transactionHash: transactionStore.keyword })
+    } else {
+      navigateToTransactionIndex()
+    }
+  } catch (error) {
+    const message = error instanceof Error ? error.message : 'Transaction does not exist.'
+    showToast(ToastType.Error, message)
+  } finally {
+    stopLoading()
+  }
+}
 </script>
