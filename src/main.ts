1d9d429ca2ffc6d7dff67217aeb3728ca4d3fffe
Muhammad Arfan <arfan2173@gmail.com>
2025-03-17 02:36:54 +0700

    added vee-validate configuration

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,17 @@ import "./assets/main.css";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { configure } from "vee-validate"
 
 import App from "./App.vue";
 import router from "./router";
+
+configure({
+  validateOnBlur: true, // controls if `blur` events should trigger validation with `handleChange` handler
+  validateOnChange: false, // controls if `change` events should trigger validation with `handleChange` handler
+  validateOnInput: false, // controls if `input` events should trigger validation with `handleChange` handler
+  validateOnModelUpdate: false, // controls if `update:modelValue` events should trigger validation with `handleChange` handler
+})
 
 const app = createApp(App);
 app.use(createPinia());
