bd4b48ff9589ed8a8299728ec04b2e7db5483a4a
Muhammad Arfan <arfan2173@gmail.com>
2025-03-17 02:54:22 +0700

    added showErrorToasts helper function

--- src/helpers/toastHelpers.ts
+++ src/helpers/toastHelpers.ts
@@ -4,3 +4,14 @@ import { useToastStore } from '@/stores/useToastStore'
 export function showToast(type: ToastType, message: string, disappearIn: number = 3000): void {
   useToastStore().addToast(type, message, disappearIn)
 }
+
+/**
+ * Shows multiple error toast messages from an array of strings.
+ * @param {string[]} errorMessages - Array of error messages to show as toasts.
+ * @param {number} [disappearIn=3000] - The duration in milliseconds before each toast disappears.
+ */
+export function showErrorToasts(errorMessages: string[], disappearIn: number = 3000): void {
+  errorMessages.forEach((errorMessage) => {
+    showToast(ToastType.Error, errorMessage, disappearIn)
+  })
+}
