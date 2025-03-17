1e3b04853a5c3f67a1814fdbe5ea27cbfe6be7c6
Muhammad Arfan <arfan2173@gmail.com>
2025-03-17 17:21:34 +0700

    updated token service

--- src/services/tokenServices.ts
+++ src/services/tokenServices.ts
@@ -1,49 +1,51 @@
 import { TransactionFailedError } from '@/errors/ethereumErrors'
 import { didTransactionFail, resolveEthersError } from '@/helpers/ethersHelpers'
-import BigNumber from 'bignumber.js'
 import { ethers } from 'ethers'
 import { EthereumAddress } from '@/interfaces/ethereumInterfaces'
 
 const tokenAbis: ethers.InterfaceAbi = [
   // name
   'function name() view returns (string)',
-
   // symbol
   'function symbol() view returns (string)',
-
   // decimals
   'function decimals() view returns (uint8)',
-
   // approve
   'function approve(address spender, uint256 value) returns (bool)',
 ]
 
-export const name = async (token: EthereumAddress, runner: ethers.BrowserProvider) => {
+export const name = async (
+  token: EthereumAddress,
+  runner: ethers.BrowserProvider,
+): Promise<string> => {
   const contract = new ethers.Contract(token, tokenAbis, runner)
   return await contract.name()
 }
 
-export const symbol = async (token: EthereumAddress, runner: ethers.BrowserProvider) => {
+export const symbol = async (
+  token: EthereumAddress,
+  runner: ethers.BrowserProvider,
+): Promise<string> => {
   const contract = new ethers.Contract(token, tokenAbis, runner)
   return await contract.symbol()
 }
 
-export const decimals = async (token: EthereumAddress, runner: ethers.BrowserProvider) => {
+export const decimals = async (
+  token: EthereumAddress,
+  runner: ethers.BrowserProvider,
+): Promise<bigint> => {
   const contract = new ethers.Contract(token, tokenAbis, runner)
   return await contract.decimals()
 }
 
 export const approve = async (
-  params: { token: string; spender: string; value: BigNumber },
+  params: { token: string; spender: string; value: bigint },
   runner: ethers.Signer,
 ): Promise<void> => {
   const contract = new ethers.Contract(params.token, tokenAbis, runner)
 
   try {
-    const tx: ethers.TransactionResponse = await contract.approve(
-      params.spender,
-      params.value.toString(),
-    )
+    const tx: ethers.TransactionResponse = await contract.approve(params.spender, params.value)
     const receipt: ethers.TransactionReceipt | null = await tx.wait()
     if (didTransactionFail(receipt)) throw new TransactionFailedError()
   } catch (error) {
