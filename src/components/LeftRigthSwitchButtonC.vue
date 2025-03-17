1b750366fb4c423e65706d6ba56cd325b8cbb5e9
Muhammad Arfan <arfan2173@gmail.com>
2025-03-18 06:27:09 +0700

    updated left right switch btn

--- src/components/LeftRigthSwitchButtonC.vue
+++ src/components/LeftRigthSwitchButtonC.vue
@@ -3,22 +3,22 @@
     <ButtonC
       class="rounded-none border-slate-700 bg-transparent px-1! font-bold text-slate-700! outline-none hover:bg-transparent hover:text-slate-700!"
       :class="{
-        'cursor-not-allowed border-b': selected === options[0],
+        'cursor-not-allowed border-b': value === options[0],
       }"
-      @onClick="selected = options[0]"
+      @onClick="updateValue(options[0])"
       type="button"
       :text="left"
-      :disabled="selected === options[0]"
+      :disabled="value === options[0]"
     />
     <ButtonC
       class="rounded-none border-slate-700 bg-transparent px-1! font-bold text-slate-700! outline-none hover:bg-transparent hover:text-slate-700!"
       :class="{
-        'cursor-not-allowed border-b': selected === options[1],
+        'cursor-not-allowed border-b': value === options[1],
       }"
-      @onClick="selected = options[1]"
+      @onClick="updateValue(options[1])"
       type="button"
       :text="right"
-      :disabled="selected === options[1]"
+      :disabled="value === options[1]"
     />
   </div>
 </template>
@@ -27,10 +27,16 @@
 import { defineProps, defineModel } from 'vue'
 import ButtonC from '@/components/ButtonC.vue'
 
-const selected = defineModel<string>({ required: true })
 const props = defineProps<{
+  value: string
   options: string[]
   left: string
   right: string
 }>()
+const emit = defineEmits<{
+  (e: 'update:value', value: string): void
+}>()
+const updateValue = (newValue: string) => {
+  emit('update:value', newValue)
+}
 </script>
