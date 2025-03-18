7c8c6692e00090fd8680f66e169ba1834cd0ab65
Muhammad Arfan <arfan2173@gmail.com>
2025-03-18 07:21:10 +0700

    updated message

--- src/validators/schemas/ethereumSchemas.ts
+++ src/validators/schemas/ethereumSchemas.ts
@@ -4,6 +4,6 @@ import { object, string, number, boolean } from 'yup'
 import { isEthAddr } from '@/utils/booleanUtils'
 
 export const ethereumAddressSchema = () =>
-  string().test('ethereum-address', '${label} invalid ethereum address.', (value) =>
+  string().test('ethereum-address', '${label} is invalid ethereum address.', (value) =>
     isEthAddr(value),
   )
