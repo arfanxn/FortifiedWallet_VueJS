4262a32eb557ae2a63575c6f958898ecfc3b941b
Muhammad Arfan <arfan2173@gmail.com>
2025-03-18 16:34:44 +0700

    renamed

---
+++ src/views/TokenView.vue
@@ -0,0 +1,25 @@
+<template>
+  <AuthLayout>
+    <div>
+      <WalletIndex
+        class="w-full md:basis-1/4"
+        v-model:keyword="walletStore.keyword"
+        v-model:currentPage="walletStore.currentPage"
+        v-model:selectedWallet="walletStore.selectedWallet"
+        :wallets="walletStore.wallets"
+        @onSelect="navigateToWalletShow"
+        @onDeselect="navigateToDashboard"
+        @onPaginate="navigateToDashboard"
+        @onFind="navigateToWalletShow"
+      />
+    </div>
+  </AuthLayout>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import AuthLayout from '@/layouts/AuthLayout.vue'
+import WalletIndex from '@/components/wallets/WalletIndex.vue'
+
+onMounted(() => {})
+</script>
