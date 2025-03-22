ba6cf5129fc38e654e80bb1e90230d3495a657aa
Muhammad Arfan <arfan2173@gmail.com>
2025-03-22 16:15:02 +0700

    removed comments

--- src/services/multicall3Services.ts
+++ src/services/multicall3Services.ts
@@ -1,4 +1,3 @@
-import { Multicall3InvalidCallData, Multicall3InvalidTargetAddress } from '@/errors/muticall3Errors'
 import { resolveEthersError } from '@/helpers/ethersHelpers'
 import { Multicall3Call, Multicall3Result } from '@/interfaces/multicall3Interfaces'
 import { ethers } from 'ethers'
@@ -9,31 +8,12 @@ const abis = [
 
 const _multicall3ContractAddr = () => import.meta.env.VITE__MULTICALL3
 
-// export const aggregate3 = (
-//   params: { calls: { target: string; allowFailure: boolean; callData: string }[] },
-//   runner: ethers.ContractRunner,
-// ) => {
-//   const contract = new ethers.Contract(_multicall3ContractAddr(), abis, runner)
-//   return contract.aggregate3(params.calls)
-// }
 
 export const aggregate3 = async (
   params: { calls: Multicall3Call[] },
   runner: ethers.BrowserProvider,
 ): Promise<Multicall3Result[]> => {
   const { calls } = params
-
-  // if (calls.length === 0) return []
-
-  // // Validate each call
-  // params.calls.forEach((call, index) => {
-  //   if (!ethers.isAddress(call.target)) {
-  //     throw new Multicall3InvalidTargetAddress()
-  //   }
-  //   if (!ethers.isBytesLike(call.callData)) {
-  //     throw new Multicall3InvalidCallData()
-  //   }
-  // })
 
   const contract = new ethers.Contract(_multicall3ContractAddr(), abis, runner)
   try {
